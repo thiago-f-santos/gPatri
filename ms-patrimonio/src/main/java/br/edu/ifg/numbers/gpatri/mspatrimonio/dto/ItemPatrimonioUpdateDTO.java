90b4017202ff7e1d1442343d4398cbf46365ac78
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-08 21:27:03 -0300

    feat: criação das DTOs de ItemPatrimonio

---
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/dto/ItemPatrimonioUpdateDTO.java
@@ -0,0 +1,18 @@
+package br.edu.ifg.numbers.gpatri.mspatrimonio.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter @Setter
+public class ItemPatrimonioUpdateDTO {
+
+    private UUID idPatrimonio;
+
+    private UUID idCondicao;
+
+    private boolean emUso;
+
+}
