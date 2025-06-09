c5ce79bcf653add2f115b6572f0b6ac431f19169
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-08 21:19:48 -0300

    feat: arquitetura inicial do microservico

---
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/MsPatrimonioApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifg.numbers.gpatri.mspatrimonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPatrimonioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPatrimonioApplication.class, args);
+    }
+
+}
---
+++ ms-patrimonio/src/test/java/br/edu/ifg/numbers/gpatri/msemprestimos/MsEmprestimosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifg.numbers.gpatri.msemprestimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmprestimosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
