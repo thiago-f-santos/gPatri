769176f6df0a3a764d7f17374e7bc2b470c9add5
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-10 09:38:28 -0300

    remove: removendo import não utilizado

--- ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/controller/CategoriaController.java
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/controller/CategoriaController.java
@@ -5,7 +5,6 @@ import br.edu.ifg.numbers.gpatri.mspatrimonio.dto.CategoriaResponseDTO;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.dto.CategoriaUpdateDTO;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.service.CategoriaService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
