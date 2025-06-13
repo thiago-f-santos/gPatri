445b98549cdb4bc6bfabf0eb24a196f468600d35
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-13 19:33:07 -0300

    chore: trocando componentModel de string por enum

--- ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/mapper/ItemPatrimonioMapper.java
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/mapper/ItemPatrimonioMapper.java
@@ -3,9 +3,10 @@ package br.edu.ifg.numbers.gpatri.mspatrimonio.mapper;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.domain.ItemPatrimonio;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.dto.ItemPatrimonioResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ItemPatrimonioMapper {
 
     ItemPatrimonioResponseDTO itemPatrimonioToPatrimonioResponseDto(ItemPatrimonio itemPatrimonio);
