deb4b6ae0e98581b88de8288c71b3d923778e7eb
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-17 19:55:49 -0300

    remove: removendo imports não utilizados

--- ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/controller/EmprestimoController.java
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/controller/EmprestimoController.java
@@ -4,7 +4,6 @@ import br.edu.ifg.numbers.gpatri.mspatrimonio.dto.*;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.service.EmprestimoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
--- ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/dto/ItemEmprestimoResponseDTO.java
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/dto/ItemEmprestimoResponseDTO.java
@@ -3,8 +3,6 @@ package br.edu.ifg.numbers.gpatri.mspatrimonio.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.UUID;
-
 @Getter @Setter
 public class ItemEmprestimoResponseDTO {
 
