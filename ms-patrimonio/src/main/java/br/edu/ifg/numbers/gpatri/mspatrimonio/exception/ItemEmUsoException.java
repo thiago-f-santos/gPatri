121e55fd3e0641c5d0e3ad353c63346fd45fabd6
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-17 19:53:06 -0300

    feat: criando exceções personalizadas

---
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/exception/ItemEmUsoException.java
@@ -0,0 +1,7 @@
+package br.edu.ifg.numbers.gpatri.mspatrimonio.exception;
+
+public class ItemEmUsoException extends RuntimeException {
+    public ItemEmUsoException(String message) {
+        super(message);
+    }
+}
