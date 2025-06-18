d03777840c2fded0532734eaf7bfd4647cc0e4dc
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-18 15:23:24 -0300

    remove: removendo imports não utilizados

--- ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/dto/ItemPatrimonioResponseDTO.java
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/dto/ItemPatrimonioResponseDTO.java
@@ -1,10 +1,6 @@
 package br.edu.ifg.numbers.gpatri.mspatrimonio.dto;
 
-import br.edu.ifg.numbers.gpatri.mspatrimonio.domain.Patrimonio;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.domain.enums.CondicaoProduto;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
