d949224ad993b57b64e26572aebcb7f9ee1f7566
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-18 15:04:08 -0300

    feat: adicionando validacoes

--- ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/dto/EmprestimoCreateDTO.java
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/dto/EmprestimoCreateDTO.java
@@ -1,5 +1,7 @@
 package br.edu.ifg.numbers.gpatri.mspatrimonio.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,12 +12,16 @@ import java.util.UUID;
 @Getter @Setter
 public class EmprestimoCreateDTO {
 
+    @NotNull(message = "O id de usuario não pode ser nulo")
     private UUID idUsuario;
 
+    @NotEmpty(message = "Para criar um empréstimo, é preciso pedir ao menos um item.")
     private List<ItemEmprestimoCreateDTO> itensEmprestimo;
 
+    @NotNull(message = "A data do empréstimo não pode ser nula")
     private Date dataEmprestimo;
 
+    @NotNull(message = "A data de devolução não pode ser nula")
     private Date dataDevolucao;
 
 }
