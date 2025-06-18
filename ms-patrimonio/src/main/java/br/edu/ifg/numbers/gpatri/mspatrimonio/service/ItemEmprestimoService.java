b76c89fe20471ba1ffef43dda441524976259da4
haterkkj <thiagoferreira2004f@gmail.com>
2025-06-18 13:51:52 -0300

    feat: implementando service contendo logica de negocio dos itens de emprestimo

--- ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/service/ItemEmprestimoService.java
+++ ms-patrimonio/src/main/java/br/edu/ifg/numbers/gpatri/mspatrimonio/service/ItemEmprestimoService.java
@@ -2,7 +2,9 @@ package br.edu.ifg.numbers.gpatri.mspatrimonio.service;
 
 import br.edu.ifg.numbers.gpatri.mspatrimonio.domain.Emprestimo;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.domain.ItemEmprestimo;
+import br.edu.ifg.numbers.gpatri.mspatrimonio.domain.ItemEmprestimoId;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.domain.ItemPatrimonio;
+import br.edu.ifg.numbers.gpatri.mspatrimonio.dto.ItemEmprestimoCreateDTO;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.dto.ItemEmprestimoResponseDTO;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.dto.ItemEmprestimoUpdateDTO;
 import br.edu.ifg.numbers.gpatri.mspatrimonio.exception.ItemEmUsoException;
@@ -17,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -31,7 +34,7 @@ public class ItemEmprestimoService {
     private final ItemPatrimonioRepository itemPatrimonioRepository;
 
     @Transactional
-    public ItemEmprestimoResponseDTO adicionaItemEmprestimo() {
+    public ItemEmprestimoResponseDTO adicionaItemEmprestimo(UUID idEmprestimo, ItemEmprestimoCreateDTO itemEmprestimoCreateDTO) {
         Emprestimo emprestimo = emprestimoRepository.findById(idEmprestimo).orElseThrow(
                 () -> new EntityNotFoundException(String.format("Emprestimo '%s' não encontrado", idEmprestimo)));
 
@@ -42,14 +45,19 @@ public class ItemEmprestimoService {
 
         if (itemPatrimonio.getQuantidade() < itemEmprestimoCreateDTO.getQuantidade()) throw new QuantidadeItemIndisponivelException(String.format("Quantidade disponivel do item patrimonio '%s' é menor que quantidade solicitada", itemEmprestimoCreateDTO.getIdItemPatrimonio()));
 
+        itemPatrimonio.setQuantidade(itemPatrimonio.getQuantidade() - itemEmprestimoCreateDTO.getQuantidade());
+        itemPatrimonio.setUpdatedAt(Instant.now());
+        itemPatrimonioRepository.save(itemPatrimonio);
+
+        ItemEmprestimoId itemEmprestimoId = new ItemEmprestimoId();
+        itemEmprestimoId.setIdEmprestimo(emprestimo.getId());
+        itemEmprestimoId.setIdItemPatrimonio(itemPatrimonio.getId());
+
         ItemEmprestimo itemEmprestimo = itemEmprestimoMapper.createDtoToItemEmprestimo(itemEmprestimoCreateDTO);
+        itemEmprestimo.setId(itemEmprestimoId);
         itemEmprestimo.setItemPatrimonio(itemPatrimonio);
         itemEmprestimo.setEmprestimo(emprestimo);
         itemEmprestimo.setCreatedAt(Instant.now());
-
-        itemPatrimonio.setQuantidade(itemPatrimonio.getQuantidade() - itemEmprestimoCreateDTO.getQuantidade());
-        itemPatrimonioRepository.save(itemPatrimonio);
-
         itemEmprestimoRepository.save(itemEmprestimo);
 
         return itemEmprestimoMapper.itemEmprestimoToItemEmprestimoResponseDTO(itemEmprestimo);
@@ -57,7 +65,60 @@ public class ItemEmprestimoService {
 
     @Transactional
     public ItemEmprestimoResponseDTO atualizaItemEmprestimo(UUID idEmprestimo, ItemEmprestimoUpdateDTO itemEmprestimoUpdateDTO) {
+        Emprestimo emprestimo = emprestimoRepository.findById(idEmprestimo).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Emprestimo '%s' não encontrado", idEmprestimo)));
 
+        ItemPatrimonio itemPatrimonio = itemPatrimonioRepository.findById(itemEmprestimoUpdateDTO.getIdItemPatrimonio()).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Item Patrimonio '%s' não encontrado", itemEmprestimoUpdateDTO.getIdItemPatrimonio())));
+
+        ItemEmprestimo itemEmprestimo = itemEmprestimoRepository.findByEmprestimo_IdEqualsAndItemPatrimonio_IdEquals(emprestimo.getId(), itemPatrimonio.getId());
+
+        if (itemEmprestimo == null) throw new EntityNotFoundException(String.format("Item patrimonio '%s' não encontrado no emprestimo '%s'", itemPatrimonio.getId(), emprestimo.getId()));
+
+        if (itemPatrimonio.getQuantidade() == 0) throw new ItemEmUsoException(String.format("Item Patrimonio '%s' ja se encontra em uso", itemEmprestimoUpdateDTO.getIdItemPatrimonio()));
+
+        if (itemPatrimonio.getQuantidade() < itemEmprestimoUpdateDTO.getQuantidade()) throw new QuantidadeItemIndisponivelException(String.format("Quantidade disponivel do item patrimonio '%s' é menor que quantidade solicitada", itemEmprestimoUpdateDTO.getIdItemPatrimonio()));
+
+        int diff = itemEmprestimo.getQuantidade() - itemEmprestimoUpdateDTO.getQuantidade();
+
+        itemPatrimonio.setQuantidade(itemPatrimonio.getQuantidade() + diff);
+        itemPatrimonio.setUpdatedAt(Instant.now());
+        itemPatrimonioRepository.save(itemPatrimonio);
+
+        itemEmprestimo.setQuantidade(itemEmprestimo.getQuantidade() + itemEmprestimoUpdateDTO.getQuantidade());
+        itemEmprestimo.setUpdatedAt(Instant.now());
+        itemEmprestimo = itemEmprestimoRepository.save(itemEmprestimo);
+
+        return itemEmprestimoMapper.itemEmprestimoToItemEmprestimoResponseDTO(itemEmprestimo);
+    }
+
+    @Transactional
+    public void apagarItemEmprestimo(UUID idEmprestimo, UUID idItemPatrimonio) {
+        ItemEmprestimo itemEmprestimo = itemEmprestimoRepository.findByEmprestimo_IdEqualsAndItemPatrimonio_IdEquals(idEmprestimo, idItemPatrimonio);
+        if (itemEmprestimo == null)
+            throw new EntityNotFoundException(String.format("Item Patrimonio '%s' no Emprestimo '%s' não encontrado", idItemPatrimonio, idEmprestimo));
+
+        ItemPatrimonio itemPatrimonio = itemPatrimonioRepository.findById(idItemPatrimonio).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Item Patrimonio '%s' não encontrado", idItemPatrimonio)));
+
+        itemPatrimonio.setQuantidade(itemPatrimonio.getQuantidade() + itemEmprestimo.getQuantidade());
+        itemPatrimonio.setUpdatedAt(Instant.now());
+        itemPatrimonioRepository.save(itemPatrimonio);
+
+        itemEmprestimoRepository.delete(itemEmprestimo);
+    }
+
+    public ItemEmprestimoResponseDTO findById(UUID idEmprestimo, UUID idItemPatrimonio) {
+        ItemEmprestimo itemEmprestimo = itemEmprestimoRepository.findByEmprestimo_IdEqualsAndItemPatrimonio_IdEquals(idEmprestimo, idItemPatrimonio);
+        if (itemEmprestimo == null)
+            throw new EntityNotFoundException(String.format("Item Emprestimo ItemPatrimonio<'%s'> no Emprestimo<'%s'> não encontrado", idItemPatrimonio, idEmprestimo));
+
+        return itemEmprestimoMapper.itemEmprestimoToItemEmprestimoResponseDTO(itemEmprestimo);
+    }
+
+    public List<ItemEmprestimoResponseDTO> findAllByEmprestimo(UUID idEmprestimo) {
+        List<ItemEmprestimo> itensEmprestimo = itemEmprestimoRepository.findAllByEmprestimo_IdEquals(idEmprestimo);
+        return itensEmprestimo.stream().map(itemEmprestimoMapper::itemEmprestimoToItemEmprestimoResponseDTO).toList();
     }
 
 }
