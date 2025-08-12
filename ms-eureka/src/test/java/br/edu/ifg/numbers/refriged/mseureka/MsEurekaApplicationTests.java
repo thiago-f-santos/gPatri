ada46d1a923cfd85c886937402ad6ba5826c481c
haterkkj <thiagoferreira2004f@gmail.com>
2025-08-12 14:53:20 -0300

    feat: implementando eureka server

---
+++ ms-eureka/src/test/java/br/edu/ifg/numbers/refriged/mseureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifg.numbers.refriged.mseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
