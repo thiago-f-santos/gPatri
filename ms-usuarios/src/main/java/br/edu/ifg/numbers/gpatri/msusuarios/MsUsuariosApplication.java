62e6f109dfeb701a353125bba9b83f721e6d1260
Thiago <thiagoferreira2004f@gmail.com>
2025-10-06 17:23:45 -0300

    feat: ativando serialização de pages

--- ms-usuarios/src/main/java/br/edu/ifg/numbers/gpatri/msusuarios/MsUsuariosApplication.java
+++ ms-usuarios/src/main/java/br/edu/ifg/numbers/gpatri/msusuarios/MsUsuariosApplication.java
@@ -2,8 +2,10 @@ package br.edu.ifg.numbers.gpatri.msusuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class MsUsuariosApplication {
 
 	public static void main(String[] args) {
