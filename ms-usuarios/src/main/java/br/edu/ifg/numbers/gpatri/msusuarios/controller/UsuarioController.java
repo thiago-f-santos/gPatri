c56bace63b52395a0b854da8eacb5b757e656c14
Thiago <thiagoferreira2004f@gmail.com>
2025-10-16 09:29:55 -0300

    feat: implementando sorting no endpoint de busca de usuarios

--- ms-usuarios/src/main/java/br/edu/ifg/numbers/gpatri/msusuarios/controller/UsuarioController.java
+++ ms-usuarios/src/main/java/br/edu/ifg/numbers/gpatri/msusuarios/controller/UsuarioController.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -67,8 +68,11 @@ public class UsuarioController {
     @GetMapping
     @PreAuthorize("hasAuthority('USUARIO_LISTAR')")
     public ResponseEntity<Page<UserResponseDTO>> buscarTodos(@RequestParam(defaultValue = "0") int page,
-                                                             @RequestParam(defaultValue = "5") int size) {
-        Pageable pageable = PageRequest.of(page, size);
+                                                             @RequestParam(defaultValue = "5") int size,
+                                                             @RequestParam(defaultValue = "createdAt") String sortBy,
+                                                             @RequestParam(defaultValue = "DESC") String direction) {
+        Sort sort = direction.equalsIgnoreCase("ASC") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
         Page<UserResponseDTO> usuarios = usuarioService.buscarTodos(pageable);
         return ResponseEntity.ok(usuarios);
     }
